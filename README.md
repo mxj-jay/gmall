f15d07bfdb41dae03f65aeb2f624d520fe2aa24f
mxj-jay <597428285@qq.com>
2020-01-09 15:49:01 +0800

    框架的基本构建, 整合dubbo

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # gmall
+
+gmall-user 用户服务(测试用)
+
+gmall-parent 版本管理
+
+gmall-api 管理项目中的bean和接口service
+
+gmall-common-util 项目中的通用工具、框架
+
+gmall-service-util service层通用的工具、框架
+
+gmall-web-util web层通用的工具、框架
+
---
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.atguigu.gmall.user.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    /**分布式整合dubbo, 需要采用dubbo的依赖注入注解*/
+    @Reference
+    UserService userService;
+
+    @RequestMapping("getReceiveAddressByMemberId")
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
+
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
+
+        return umsMemberReceiveAddresses;
+    }
+
+
+    @RequestMapping("getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+
+        List<UmsMember> umsMembers = userService.getAllUser();
+
+        return umsMembers;
+    }
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "hello user";
+    }
+
+
+
+}
